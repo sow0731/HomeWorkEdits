f400a0726b1534ed61c2e206c90a47a1468cc497
sow0731 <sow.ishihara@gmail.com>
2022-04-19 23:03:10 +0900

    課題再提出

---
+++ HomeWorksEdits/src/Kadai5.java
@@ -0,0 +1,14 @@
+
+public class Kadai5 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 20; i++) {
+			if (i < 10) {
+				System.out.println("HelloWorld");
+			} else {
+				System.out.println("Hello");
+			}
+		}
+	}
+
+}

2b842a3b4e2249e541cfe663ef47aaf66039c507
sow0731 <sow.ishihara@gmail.com>
2022-04-20 20:41:52 +0900

    RequestTest2

--- HomeWorksEdits/src/Kadai5.java
+++ HomeWorksEdits/src/Kadai5.java
@@ -3,7 +3,7 @@ public class Kadai5 {
 
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
-		System.out.println("RequestTest");
+		System.out.println("RequestTest2");
 
 		for (int i = 1; i <= 20; i++) {
 			if (i < 10) {

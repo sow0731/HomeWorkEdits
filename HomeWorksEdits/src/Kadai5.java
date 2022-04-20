3079e176f26a7fae9cc7b1ba7a02923ceaa40e0c
sow0731 <sow.ishihara@gmail.com>
2022-04-20 20:32:17 +0900

    PullRequestTest

--- HomeWorksEdits/src/Kadai5.java
+++ HomeWorksEdits/src/Kadai5.java
@@ -2,6 +2,8 @@
 public class Kadai5 {
 
 	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+
 		for (int i = 1; i <= 20; i++) {
 			if (i < 10) {
 				System.out.println("HelloWorld");
